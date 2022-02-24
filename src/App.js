c021ade227362880efff49e1b3d37427e92b424c
Surendra <v-surendra.raj@shutterfly.com>
2022-02-24 13:47:45 +0530

    added routes and links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Profile from "./Profile";
 
 function App() {
   return (
-   <div>hello</div>
+    <div>React Routing{" "}
+      <Link to="/">Home</Link> |{" "}
+
+      <Link to="/about">About</Link> |{" "}
+      <Link to="/profile">Profile</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+
+    </div>
   );
 }
 
